e12a03185287e8963af844a498dfe12af68619ab
João Inez <joaoinez.info@gmail.com>
2019-08-22 17:06:57 +0100

    Touches up styling on form

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -28,21 +28,21 @@ const PhoneIcon = styled(Phone)`
   margin-right: 10px;
 `;
 
-// const HammerIcon = styled(Hammer)`
-//   width: 30px;
-//   color: ${({ theme }) => theme.darkGrey};
-// `;
+const HammerIcon = styled(Hammer)`
+  width: 30px;
+  color: ${({ theme }) => theme.darkGrey};
+`;
 
-// const Logo = styled.div`
-//   border: 2.5px solid ${({ theme }) => theme.darkGrey};
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   border-radius: 50%;
-//   width: 60px;
-//   height: 60px;
-//   margin-right: 10px;
-// `;
+const Logo = styled.div`
+  border: 2.5px solid ${({ theme }) => theme.darkGrey};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  margin-right: 10px;
+`;
 
 const Nav = styled.nav`
   display: flex;
@@ -64,7 +64,7 @@ const Nav = styled.nav`
 `;
 
 const FormTitle = styled(H)`
-  margin: 0 0 10px 0;
+  margin: 0 0 20px 0;
 `;
 
 const Form = styled.form`
@@ -83,6 +83,7 @@ const Input = styled.input`
   margin-bottom: 5px;
   padding: 10px;
   color: ${({ theme }) => theme.white};
+  box-sizing: border-box;
 `;
 
 const TextArea = styled.textarea`
@@ -95,36 +96,48 @@ const TextArea = styled.textarea`
   margin-bottom: 5px;
   padding: 10px;
   color: ${({ theme }) => theme.white};
+  box-sizing: border-box;
 `;
 
 const Footer = () => (
   <SFooter>
     <Row maxWidth="1000px" margin="0 auto">
-      <Col flex column>
-        <Info>
-          <AtIcon />
-          <P color="white">woodconstroi@gmail.com</P>
-        </Info>
-        <Info>
-          <PhoneIcon />
-          <P color="white">+351 912 345 678</P>
-        </Info>
+      <Col>
+        <Row margin="0 0 20px 0">
+          <Col flex column>
+            <Info>
+              <AtIcon />
+              <P color="white">woodconstroi@gmail.com</P>
+            </Info>
+            <Info>
+              <PhoneIcon />
+              <P color="white">+351 912 345 678</P>
+            </Info>
+          </Col>
+          <Col>
+            <Nav>
+              <Link href="/">
+                <a>
+                  <P color="white">Home</P>
+                </a>
+              </Link>
+              <Link href="/portfolio">
+                <a>
+                  <P color="white">Portfolio</P>
+                </a>
+              </Link>
+            </Nav>
+          </Col>
+        </Row>
+        <Row>
+          <Col flex align="center" justify="center" sm={0}>
+            <Logo>
+              <HammerIcon />
+            </Logo>
+          </Col>
+        </Row>
       </Col>
       <Col>
-        <Nav>
-          <Link href="/">
-            <a>
-              <P color="white">Home</P>
-            </a>
-          </Link>
-          <Link href="/portfolio">
-            <a>
-              <P color="white">Portfolio</P>
-            </a>
-          </Link>
-        </Nav>
-      </Col>
-      <Col md={2} lg={2}>
         <FormTitle h={3} color="white">
           Contacte-nos
         </FormTitle>
