61cd5ac5cfd3a8a17e4d20bc95fe34f09cf48afd
João Inez <joaoinez.info@gmail.com>
2019-08-22 10:53:21 +0100

    Netlify form test

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -99,21 +99,26 @@ const Footer = () => (
       <input type="hidden" name="form-name" value="contact" />
       <p>
         <label>
-          Your Name: <input type="text" name="name" />
+          Nome: <input type="text" name="name" />
         </label>
       </p>
       <p>
         <label>
-          Your Email: <input type="email" name="email" />
+          Assunto: <input type="text" name="about" />
         </label>
       </p>
       <p>
         <label>
-          Message: <textarea name="message" />
+          Email: <input type="email" name="email" />
         </label>
       </p>
       <p>
-        <button type="submit">Send</button>
+        <label>
+          Mensagem: <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Enviar</button>
       </p>
     </form>
   </SFooter>
