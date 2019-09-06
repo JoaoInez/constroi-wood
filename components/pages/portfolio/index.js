c827878f0d11f0fd03dddfb0715d8fc386776675
João Inez <joaoinez.info@gmail.com>
2019-09-06 23:06:06 +0100

    Refactors code to use the content created by the CMS

--- components/pages/portfolio/index.js
+++ components/pages/portfolio/index.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import * as R from "ramda";
 import { Section, H, Divider } from "../../ui";
-import { carpentry, painting, remodeling } from "../../../data/portfolio.json";
-// import carpentry from "../../../content/portfolio/carpentry.md";
-// import painting from "../../../content/portfolio/painting.md";
-// import remodeling from "../../../content/portfolio/remodeling.md";
+import carpentryContent from "../../../content/portfolio/carpentry.md";
+import paintingContent from "../../../content/portfolio/painting.md";
+import remodelingContent from "../../../content/portfolio/remodeling.md";
 import Card from "./Card";
 import Gallery from "./Gallery";
+
+const carpentry = R.path(["attributes", "carpentry"])(carpentryContent);
+const painting = R.path(["attributes", "painting"])(paintingContent);
+const remodeling = R.path(["attributes", "remodeling"])(remodelingContent);
 
 const Row = styled.div`
   display: flex;
@@ -27,23 +31,26 @@ const Portfolio = () => {
         <H>Carpintaria</H>
         <Divider width="33%" margin="0 0 20px 0" />
         <Row>
-          {carpentry.map(({ images, title }, i) => (
-            <Card key={i} images={images} title={title} open={setOpen} />
-          ))}
+          {carpentry &&
+            carpentry.map(({ gallery, name }, i) => (
+              <Card key={i} images={gallery} title={name} open={setOpen} />
+            ))}
         </Row>
         <H>Pinturas</H>
         <Divider width="33%" margin="0 0 20px 0" />
         <Row>
-          {painting.map(({ images, title }, i) => (
-            <Card key={i} images={images} title={title} open={setOpen} />
-          ))}
+          {painting &&
+            painting.map(({ gallery, name }, i) => (
+              <Card key={i} images={gallery} title={name} open={setOpen} />
+            ))}
         </Row>
         <H>Remodelações</H>
         <Divider width="33%" margin="0 0 20px 0" />
         <Row>
-          {remodeling.map(({ images, title }, i) => (
-            <Card key={i} images={images} title={title} open={setOpen} />
-          ))}
+          {remodeling &&
+            remodeling.map(({ gallery, name }, i) => (
+              <Card key={i} images={gallery} title={name} open={setOpen} />
+            ))}
         </Row>
       </Section>
       <Gallery open={open.state} close={setOpen} images={open.images} />
