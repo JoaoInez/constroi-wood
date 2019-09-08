cbef9440a7c1b5d621dd43c3b103d329f66d6bb7
João Inez <joaoinez.info@gmail.com>
2019-09-08 20:04:25 +0100

    Fixes whitespace in meta data

--- components/shared/Meta.js
+++ components/shared/Meta.js
@@ -12,11 +12,17 @@ const Meta = () => {
       />
       <meta
         name="description"
-        content={R.pathOr("", ["attributes", "description"])(content)}
+        content={R.compose(
+          R.replace(/\u00a0/g, " "),
+          R.pathOr("", ["attributes", "description"])
+        )(content)}
       />
       <meta
         name="keywords"
-        content={R.pathOr("", ["attributes", "keywords"])(content)}
+        content={R.compose(
+          R.replace(/\u00a0/g, " "),
+          R.pathOr("", ["attributes", "keywords"])
+        )(content)}
       />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <title>ConstroiWood</title>
